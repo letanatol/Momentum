93a0cf695800a374ee6e9658e10888466014558c
letanatol <letanatol@gmail.com>
2024-09-25 02:03:06 +0300

    feat: add weather block

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const backgroundElement = document.querySelector('.bg');
 const clockElement = document.querySelector('.clock');
 const dateElement = document.querySelector('.date');
-const backgroundElement = document.querySelector('.bg');
+const cityElement = document.querySelector('.city');
+const tempElement = document.querySelector('.weather-temperature');
+const descriptionElement = document.querySelector('.weather-description');
+
+const WEATHER_API_KEY = 'a641e487284291b3b714caeac6f11871';
+const myCity = 'Krasnodar';
 
 function addZero(n) {
   return (parseInt(n, 10) < 10 ? "0" : "") + n;
@@ -38,4 +44,58 @@ function showTimeDateBlock() {
   changeBackground(hours);
 }
 
+function getPositionFromLocalStorage() {
+  const storedCity = localStorage.getItem('position');
+  return storedCity ? JSON.parse(storedCity) : { lat: 45.0355, lon: 38.9753 };
+}
+
+function setPositionToLocalStorage(latitude, longitude) {
+  localStorage.setItem('position', JSON.stringify({ lat: latitude, lon: longitude }));
+}
+
+function getPositionByGeolocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      setPositionToLocalStorage(latitude, longitude);
+
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    }, (error) => {
+      console.error(`Error occurred. Error code: ${error.code}`);
+    });
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+}
+
+async function getWeather() {
+  const currentPosition = getPositionFromLocalStorage();
+
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${currentPosition.lat}&lon=${currentPosition.lon}&appid=${WEATHER_API_KEY}&units=metric`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (response.ok) {
+      const temp = data.main.temp;
+      const description = data.weather[0].description;
+
+      cityElement.textContent = data.name;
+      tempElement.textContent = `${data.main.temp.toFixed(0)}°C`;
+      descriptionElement.textContent = description;
+      console.log('weather: ', data);
+    } else {
+      console.log('Error:', data.message);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+showTimeDateBlock();
 setInterval(showTimeDateBlock, 1000);
+getPositionByGeolocation();
+getWeather();
