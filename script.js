2c285de2fbaf3bd5f3d03f91bd129f9346f69911
letanatol <letanatol@gmail.com>
2024-09-24 23:59:26 +0300

    feat: implement dynamic wallpaper change according to the current time

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const clockElement = document.querySelector('.clock');
-const dateElement  = document.querySelector('.date');
+const dateElement = document.querySelector('.date');
+const backgroundElement = document.querySelector('.bg');
+
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+function changeBackground(hours) {
+  if (hours >= 0 && hours < 6) {
+    backgroundElement.style.backgroundImage = 'url("assets/images/01.jpg")';
+  } else if (hours >= 6 && hours < 12) {
+    backgroundElement.style.backgroundImage = 'url("assets/images/02.jpg")';
+  } else if (hours >= 12 && hours < 18) {
+    backgroundElement.style.backgroundImage = 'url("assets/images/03.jpg")';
+  } else {
+    backgroundElement.style.backgroundImage = 'url("assets/images/04.jpg")';
+  }
+}
 
 function showTimeDateBlock() {
   const currentTimestamp = new Date();
@@ -17,11 +34,8 @@ function showTimeDateBlock() {
   const currentDate = `${currentTimestamp.toLocaleDateString("ru-RU", optionsDate)}, ${currentTimestamp.toLocaleDateString("ru-RU", { weekday: 'long' })}`
 
   clockElement.textContent = currentTime;
-  dateElement .textContent = currentDate;
+  dateElement.textContent = currentDate;
+  changeBackground(hours);
 }
 
 setInterval(showTimeDateBlock, 1000);
-
-function addZero(n) {
-  return (parseInt(n, 10) < 10 ? "0" : "") + n;
-}
